50464f9ccf7ff1ed4d54b398a40aae7c96c9b3d2
connektind <viky@connekt.in>
2020-09-22 19:04:21 +0530

    new

---
+++ CRM/CRM/Model/LeadbulkuploadModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.Model
+{
+    public class LeadbulkuploadModel
+    {
+        public string ResponseMessage { get; set; }
+    }
+}
